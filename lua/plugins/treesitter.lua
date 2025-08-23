3cab037c79e95796016aa00dc8bcc4d59065a704
pfaj <pfaj2004@gmail.com>
2025-08-22 23:07:18 -0400

    removed treesitter for testing purposes with bugs and updates

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,59 +1,60 @@
-return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
-  keys = {
-    { "<c-space>", desc = "Increment Selection" },
-    { "<bs>", desc = "Decrement Selection", mode = "x" },
-  },
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      highlight = { enable = true },
-      indent = { enable = true },
-      ensure_installed = {
-        "bash",
-        "c",
-        "diff",
-        "html",
-        "javascript",
-        "jsdoc",
-        "json",
-        "jsonc",
-        "lua",
-        "luadoc",
-        "luap",
-        "markdown",
-        "markdown_inline",
-        "printf",
-        "python",
-        "query",
-        "regex",
-        "toml",
-        "tsx",
-        "typescript",
-        "vim",
-        "vimdoc",
-        "xml",
-        "yaml",
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
-        },
-      },
-      textobjects = {
-        move = {
-          enable = true,
-          goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer", ["]a"] = "@parameter.inner" },
-          goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
-          goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer", ["[a"] = "@parameter.inner" },
-          goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer", ["[A"] = "@parameter.inner" },
-        },
-      },
-    })
-  end,
-}
+return {}
+-- return {
+--   "nvim-treesitter/nvim-treesitter",
+--   build = ":TSUpdate",
+--   cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+--   keys = {
+--     { "<c-space>", desc = "Increment Selection" },
+--     { "<bs>", desc = "Decrement Selection", mode = "x" },
+--   },
+--   config = function()
+--     require("nvim-treesitter.configs").setup({
+--       highlight = { enable = true },
+--       indent = { enable = true },
+--       ensure_installed = {
+--         "bash",
+--         "c",
+--         "diff",
+--         "html",
+--         "javascript",
+--         "jsdoc",
+--         "json",
+--         "jsonc",
+--         "lua",
+--         "luadoc",
+--         "luap",
+--         "markdown",
+--         "markdown_inline",
+--         "printf",
+--         "python",
+--         "query",
+--         "regex",
+--         "toml",
+--         "tsx",
+--         "typescript",
+--         "vim",
+--         "vimdoc",
+--         "xml",
+--         "yaml",
+--       },
+--       incremental_selection = {
+--         enable = true,
+--         keymaps = {
+--           init_selection = "<C-space>",
+--           node_incremental = "<C-space>",
+--           scope_incremental = false,
+--           node_decremental = "<bs>",
+--         },
+--       },
+--       textobjects = {
+--         move = {
+--           enable = true,
+--           goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer", ["]a"] = "@parameter.inner" },
+--           goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
+--           goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer", ["[a"] = "@parameter.inner" },
+--           goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer", ["[A"] = "@parameter.inner" },
+--         },
+--       },
+--     })
+--   end,
+-- }
